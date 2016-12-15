31b9057539c2304f8475f1b53b89208688957b84
Thomas Hagan <thlinux@gmail.com>
2016-12-15 14:02:27 -0500

    Initial file upload

---
+++ DirectAPI_Csharp_Sample/DirectAPI_Csharp_Sample/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DirectAPI_Csharp_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btProcess = new System.Windows.Forms.Button();
+            this.txtJSONRequest = new System.Windows.Forms.TextBox();
+            this.txtJSONResponse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btLoadVaultRequest = new System.Windows.Forms.Button();
+            this.btLoadSaleRequest = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btLoadGetDetail = new System.Windows.Forms.Button();
+            this.txtVerb = new System.Windows.Forms.TextBox();
+            this.txtReference = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btProcess
+            // 
+            this.btProcess.Location = new System.Drawing.Point(11, 330);
+            this.btProcess.Margin = new System.Windows.Forms.Padding(2);
+            this.btProcess.Name = "btProcess";
+            this.btProcess.Size = new System.Drawing.Size(116, 28);
+            this.btProcess.TabIndex = 3;
+            this.btProcess.Text = "Process Request";
+            this.btProcess.UseVisualStyleBackColor = true;
+            this.btProcess.Click += new System.EventHandler(this.btProcess_Click);
+            // 
+            // txtJSONRequest
+            // 
+            this.txtJSONRequest.Location = new System.Drawing.Point(216, 146);
+            this.txtJSONRequest.Margin = new System.Windows.Forms.Padding(2);
+            this.txtJSONRequest.Multiline = true;
+            this.txtJSONRequest.Name = "txtJSONRequest";
+            this.txtJSONRequest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJSONRequest.Size = new System.Drawing.Size(227, 245);
+            this.txtJSONRequest.TabIndex = 5;
+            // 
+            // txtJSONResponse
+            // 
+            this.txtJSONResponse.Location = new System.Drawing.Point(447, 146);
+            this.txtJSONResponse.Margin = new System.Windows.Forms.Padding(2);
+            this.txtJSONResponse.Multiline = true;
+            this.txtJSONResponse.Name = "txtJSONResponse";
+            this.txtJSONResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJSONResponse.Size = new System.Drawing.Size(227, 245);
+            this.txtJSONResponse.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Request";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 131);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Response";
+            // 
+            // btLoadVaultRequest
+            // 
+            this.btLoadVaultRequest.Location = new System.Drawing.Point(11, 72);
+            this.btLoadVaultRequest.Margin = new System.Windows.Forms.Padding(2);
+            this.btLoadVaultRequest.Name = "btLoadVaultRequest";
+            this.btLoadVaultRequest.Size = new System.Drawing.Size(139, 28);
+            this.btLoadVaultRequest.TabIndex = 2;
+            this.btLoadVaultRequest.Text = "Load Vault Request";
+            this.btLoadVaultRequest.UseVisualStyleBackColor = true;
+            this.btLoadVaultRequest.Click += new System.EventHandler(this.btLoadVaultRequest_Click);
+            // 
+            // btLoadSaleRequest
+            // 
+            this.btLoadSaleRequest.Location = new System.Drawing.Point(11, 23);
+            this.btLoadSaleRequest.Margin = new System.Windows.Forms.Padding(2);
+            this.btLoadSaleRequest.Name = "btLoadSaleRequest";
+            this.btLoadSaleRequest.Size = new System.Drawing.Size(139, 28);
+            this.btLoadSaleRequest.TabIndex = 1;
+            this.btLoadSaleRequest.Text = "Load Sale Request";
+            this.btLoadSaleRequest.UseVisualStyleBackColor = true;
+            this.btLoadSaleRequest.Click += new System.EventHandler(this.btLoadSaleRequest_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(216, 23);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(458, 45);
+            this.txtURL.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "URL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(355, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Note: Request may be edited prior to selecting \"Process Request\" button.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "- OR -";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "- OR -";
+            // 
+            // btLoadGetDetail
+            // 
+            this.btLoadGetDetail.Location = new System.Drawing.Point(11, 123);
+            this.btLoadGetDetail.Margin = new System.Windows.Forms.Padding(2);
+            this.btLoadGetDetail.Name = "btLoadGetDetail";
+            this.btLoadGetDetail.Size = new System.Drawing.Size(139, 28);
+            this.btLoadGetDetail.TabIndex = 3;
+            this.btLoadGetDetail.Text = "Load Get Charges Detail";
+            this.btLoadGetDetail.UseVisualStyleBackColor = true;
+            this.btLoadGetDetail.Click += new System.EventHandler(this.btLoadGetDetail_Click);
+            // 
+            // txtVerb
+            // 
+            this.txtVerb.Location = new System.Drawing.Point(216, 99);
+            this.txtVerb.Name = "txtVerb";
+            this.txtVerb.Size = new System.Drawing.Size(227, 20);
+            this.txtVerb.TabIndex = 14;
+            // 
+            // txtReference
+            // 
+            this.txtReference.Location = new System.Drawing.Point(447, 99);
+            this.txtReference.Name = "txtReference";
+            this.txtReference.Size = new System.Drawing.Size(227, 20);
+            this.txtReference.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(214, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Verb";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(445, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Reference";
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(11, 363);
+            this.btClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(116, 28);
+            this.btClear.TabIndex = 19;
+            this.btClear.Text = "Clear Form";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 428);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtReference);
+            this.Controls.Add(this.txtVerb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btLoadGetDetail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btLoadSaleRequest);
+            this.Controls.Add(this.btLoadVaultRequest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtJSONResponse);
+            this.Controls.Add(this.txtJSONRequest);
+            this.Controls.Add(this.btProcess);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Direct API C# Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btProcess;
+        private System.Windows.Forms.TextBox txtJSONRequest;
+        private System.Windows.Forms.TextBox txtJSONResponse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btLoadVaultRequest;
+        private System.Windows.Forms.Button btLoadSaleRequest;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btLoadGetDetail;
+        private System.Windows.Forms.TextBox txtVerb;
+        private System.Windows.Forms.TextBox txtReference;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btClear;
+    }
+}
+
---
+++ DirectAPI_Csharp_Sample/DirectAPI_Csharp_Sample/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace DirectAPI_Csharp_Sample
+{
+    //=====================================================
+    //Sample Direct API request - C#.net
+    //Thomas Hagan
+    //Integration Consultant
+    //Sage Payment Solutions
+    //November 22nd, 2016
+    //Application is intended for instructional use only.
+    //If you have any questions, please feel free to email
+    //the Integration Support team at sdksupport@sage.com
+    //Also, please make sure to register for API credentials
+    //at our developer portal: https://developer.sagepayments.com
+    //======================================================
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btProcess_Click(object sender, EventArgs e)
+        {
+            //TH - Test Data. This is the test account infomation we provide
+            //in the API Sandbox. Please contact us at sdksupport@sage.com if
+            //you need a unique test account and receive the Merchant ID and
+            //Merchant Key. In order to get your own Client ID and Client Secret
+            //you must register at https://developer.sagepayments.com and setup
+            //an App under My Apps. Please let us know if you have any questions.
+            string merchantId = "173859436515";
+            string merchantKey = "P1J2V8P2Q3D8";
+
+            //TH - The Client ID and Client Key should be hard coded and not displayed
+            //in the production product. These are your API credentials used for
+            //security and tracking purposes.
+            string clientId = "W8yvKQ5XbvAn7dUDJeAnaWCEwA4yXEgd";
+            string clientSecret = "iLzODV5AUsCGWGkr";
+
+            //TH - http://requestb.in is a good resource for troubleshooting.
+            string strUrl = txtURL.Text;
+
+            string verb = txtVerb.Text;
+            string contentType = "application/json";
+            string strRequest = txtJSONRequest.Text;
+            var nl = Environment.NewLine;
+            
+            //TH - Building timestamp. Needs to be epoch in seconds.
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+            string timestamp = t.TotalSeconds.ToString();
+            //TH - Previously used timestamp for Nonce, changed to Guid.
+            //Nonce should be a unique value per transaction.
+            Guid g = Guid.NewGuid();
+            string nonce = g.ToString();
+
+
+            //TH - Build the Authorization
+            string authToken = verb + strUrl + strRequest + merchantId + nonce + timestamp;
+            byte[] hash_authToken = new HMACSHA512(Encoding.ASCII.GetBytes(clientSecret)).ComputeHash(Encoding.ASCII.GetBytes(authToken));
+            string hash64_authToken = Convert.ToBase64String(hash_authToken);
+
+
+            //TH - Make sure the URL string has been included.
+            if (String.IsNullOrEmpty(strUrl) == true)
+            {
+                txtJSONResponse.Text = "Error: Please make sure the URL field is not blank.";
+                return;
+            }
+
+            //TH - Send the request
+            if (verb == "GET")
+            {
+                request_GET(verb, strUrl, merchantId, merchantKey, nonce, timestamp, clientId, clientSecret);
+            }
+            else
+            {
+                request_POST(verb, strUrl, strRequest, merchantId, merchantKey, nonce, timestamp, clientId, clientSecret, contentType);
+            }
+            
+            
+        }
+
+        private void btLoadVaultRequest_Click(object sender, EventArgs e)
+        {
+            //TH - Request variables
+            string strCardNumber = "5454545454545454";
+            string strExpDate = "0920";
+            string strURL = "https://api-cert.sagepayments.com/token/v1/tokens";
+            string strVerb = "POST";
+
+            //TH - load request message
+            string strRequestVault =
+                "{\"cardData\":{\"number\":\"" + strCardNumber +
+                "\",\"expiration\":\"" + strExpDate +
+                "\"}}";
+
+            txtJSONRequest.Text = strRequestVault;
+            txtURL.Text = strURL;
+            txtVerb.Text = strVerb;
+        }
+
+        private void btLoadSaleRequest_Click(object sender, EventArgs e)
+        {
+
+            //TH - Request variables
+            string strOrderNumber = "Invoice " + DateTime.Now.Millisecond;
+            string strTotalAmount = "7";
+            string strCardNumber = "5454545454545454";
+            string strExpDate = "0920";
+            string strURL = "https://api-cert.sagepayments.com/bankcard/v1/charges?type=Sale";
+            string strVerb = "POST";
+
+            //!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //TH - This is the bare minimum request for a Sale or Authorization
+            //More information may be needed to meet the requirements for a 
+            //qualified transaction. Most card-not-present transactions and transactions
+            //where the card was entered manually will require the addition of the street
+            //address, zip code, and the cvv value to qualify for the best rate.
+            //!!!!!!!!!!!!!!!!!!!!!>>>IMPORTANT<<<!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            //TH - load request message
+            string strRequest =
+                "{\"Ecommerce\":{\"OrderNumber\":\"" + strOrderNumber +
+                "\",\"Amounts\":{\"Total\":\"" + strTotalAmount +
+                "\"},\"CardData\":{\"Number\":\"" + strCardNumber +
+                "\",\"Expiration\":\"" + strExpDate +
+                "\"}}}";
+
+            txtJSONRequest.Text = strRequest;
+            txtURL.Text = strURL;
+            txtVerb.Text = strVerb;
+        }
+
+        private void btLoadGetDetail_Click(object sender, EventArgs e)
+        {
+            //TH - Request variables
+            string strURL = "https://api-cert.sagepayments.com/bankcard/v1/charges/";
+            //TH - There is no request string with a GET Charges Detail request.
+            string strRequest = "";
+            string strVerb = "GET";
+            string strReference = "DC6GFbyDO0";
+
+            txtJSONRequest.Text = strRequest;
+            txtURL.Text = strURL + strReference;
+            txtVerb.Text = strVerb;
+            txtReference.Text = strReference;
+        }
+
+        private void request_POST(string verb, string strUrl, string strRequest, string merchantId, string merchantKey, string nonce, string timestamp, string clientId, string clientSecret, string contentType)
+        {
+            var nl = Environment.NewLine;
+
+            //TH - Build the Authorization
+            string authToken = verb + strUrl + strRequest + merchantId + nonce + timestamp;
+            byte[] hash_authToken = new HMACSHA512(Encoding.ASCII.GetBytes(clientSecret)).ComputeHash(Encoding.ASCII.GetBytes(authToken));
+            string hash64_authToken = Convert.ToBase64String(hash_authToken);
+
+
+            //TH - Make sure the URL string has been included.
+            if (String.IsNullOrEmpty(strUrl) == true)
+            {
+                txtJSONResponse.Text = "Error: Please make sure the URL field is not blank.";
+                return;
+            }
+
+            //TH - Initiate Web Request
+            var web_request = (HttpWebRequest)WebRequest.Create(strUrl);
+
+            //TH - Set the headers and details
+            var web_request_headers = web_request.Headers;
+            Console.WriteLine("Configuring web_request to include headers for Sage Direct API");
+            web_request_headers["clientId"] = clientId;
+            web_request_headers["merchantId"] = merchantId;
+            web_request_headers["merchantKey"] = merchantKey;
+            web_request_headers["nonce"] = nonce;
+            web_request_headers["timestamp"] = timestamp;
+            web_request_headers["authorization"] = hash64_authToken;
+            web_request.Method = verb;
+
+            //TH - Format the request
+            var byteArray = Encoding.ASCII.GetBytes(strRequest);
+
+            //TH - Send the data
+            web_request.ContentType = contentType;
+            web_request.ContentLength = byteArray.Length;
+            var datastream = web_request.GetRequestStream();
+            datastream.Write(byteArray, 0, byteArray.Length);
+            datastream.Close();
+
+            //TH - Get the Response and Catch any errors.
+            try
+            {
+                var web_response = (HttpWebResponse)web_request.GetResponse();
+                Console.WriteLine(web_response.StatusDescription);
+                datastream = web_response.GetResponseStream();
+
+                var reader = new StreamReader(datastream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                reader.Close();
+                datastream.Close();
+                web_response.Close();
+
+                //TH - Display response.
+                txtJSONResponse.Text = "Server Status Code: " + web_response.StatusCode + nl + "Server Status: " + web_response.StatusDescription + nl + "API Response: " + responseFromServer;
+
+            }
+            catch (Exception exception)
+            {
+                txtJSONResponse.Text = "Server Response: " + exception.Message;
+            }
+        }
+
+        private void request_GET(string verb, string strUrl, string merchantId, string merchantKey, string nonce, string timestamp, string clientId, string clientSecret)
+        {
+            var nl = Environment.NewLine;
+
+            //TH - Build the Authorization
+            string authToken = verb + strUrl + merchantId + nonce + timestamp;
+            byte[] hash_authToken = new HMACSHA512(Encoding.ASCII.GetBytes(clientSecret)).ComputeHash(Encoding.ASCII.GetBytes(authToken));
+            string hash64_authToken = Convert.ToBase64String(hash_authToken);
+
+
+            //TH - Make sure the URL string has been included.
+            if (String.IsNullOrEmpty(strUrl) == true)
+            {
+                txtJSONResponse.Text = "Error: Please make sure the URL field is not blank.";
+                return;
+            }
+
+            //TH - Initiate Web Request
+            var web_request = (HttpWebRequest)WebRequest.Create(strUrl);
+
+            //TH - Set the headers and details
+            var web_request_headers = web_request.Headers;
+            Console.WriteLine("Configuring web_request to include headers for Sage Direct API");
+            web_request_headers["clientId"] = clientId;
+            web_request_headers["merchantId"] = merchantId;
+            web_request_headers["merchantKey"] = merchantKey;
+            web_request_headers["nonce"] = nonce;
+            web_request_headers["timestamp"] = timestamp;
+            web_request_headers["authorization"] = hash64_authToken;
+            web_request.Method = verb;
+
+            //TH - Get the Response and Catch any errors.
+            try
+            {
+                var web_response = (HttpWebResponse)web_request.GetResponse();
+                Console.WriteLine(web_response.StatusDescription);
+                var datastream = web_response.GetResponseStream();
+
+                var reader = new StreamReader(datastream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                reader.Close();
+                datastream.Close();
+                web_response.Close();
+
+                //TH - Display response.
+                txtJSONResponse.Text = "Server Status Code: " + web_response.StatusCode + nl + "Server Status: " + web_response.StatusDescription + nl + "API Response: " + responseFromServer;
+
+            }
+            catch (Exception exception)
+            {
+                txtJSONResponse.Text = "Server Response: " + exception.Message;
+            }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            //TH - Clear Fields
+            txtJSONRequest.Text = "";
+            txtJSONResponse.Text = "";
+            txtReference.Text = "";
+            txtURL.Text = "";
+            txtVerb.Text = "";
+        }
+    }
+}
